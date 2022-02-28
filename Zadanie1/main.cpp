568aa5cfde67ee01f1de67ed2a8312eb7fb20fe9
StepkaPopka12345 <Vitaminka.Flex@yandex.ru>
2022-02-28 13:26:13 +0300

    Week 1-2

---
+++ Zadanie1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std:: cout << " ***** " << std::endl
+               << "*      " << std::endl
+               << "*      " << std::endl
+               << "*      " << std::endl
+               << "*      " << std::endl
+               << " *      " << std::endl
+               << "  **** " << std::endl;
+    std::getchar();
+    return 0;
+}
---
+++ Zadanie3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float quadrat,qub,a;
+    std:: cin >> a;
+    quadrat=a*a;
+    qub=a*a*a;
+    cout << quadrat <<" " << qub <<endl;
+    return 0;
+}
